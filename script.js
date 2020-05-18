5449a12624bc518c62830e9967554eb6dbb16f90
oturchyn <olenka0601@meta.ua>
2020-05-18 17:35:54 +0000

    add script

---
+++ script.js
@@ -0,0 +1,8 @@
+function download(){
+    let btn = document.querySelector('.form__submit');
+    btn.addEventListener('click', function(){
+        alert('Oops... something went wrong')
+    })
+}
+
+download();
